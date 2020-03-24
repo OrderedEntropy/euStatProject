cb3a3a0303ce94c52d3f012fc074ea92d1b0b932
Zahidul Hoque <zahidul23@gmail.com>
2020-03-24 19:53:56 -0400

    fixed downloading file

--- src/main/java/data/Downloader.java
+++ src/main/java/data/Downloader.java
@@ -1,24 +1,17 @@
 package data;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Paths;
 
-
-import org.apache.commons.io.FileUtils;
 public class Downloader {
 	private File f;
 	
-	public File getFile(String url) {
-		try {
-			FileUtils.copyURLToFile(new URL(url), f);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public File getFile(String st) throws MalformedURLException, URISyntaxException {
+		URL url = new URL(st);
+		f = Paths.get(url.toURI().getPath()).toFile();
 		return f;
 	}
 }
--- src/main/java/data/ReadData.java
+++ src/main/java/data/ReadData.java
@@ -1,10 +1,12 @@
 package data;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 
 public class ReadData {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException, URISyntaxException {
 		Downloader dl = new Downloader();
 		File f = dl.getFile("https://ec.europa.eu/budget/library/figures/internet-tables-all.xls");
 		
