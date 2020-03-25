0a7cc55bcc5b9b0ba7780cb1907c65d8bf625ac1
JasonAg <jason.aguilera.s@gmail.com>
2020-03-24 20:29:47 -0400

    Added yearToYearDiff() to GrowthData.class

--- src/main/java/data/GrowthData.java
+++ src/main/java/data/GrowthData.java
@@ -24,7 +24,7 @@ public class GrowthData {
 	/**
 	 * Iterate through growthValues and check whether values are increasing or not
 	 * 
-	 * @return whether growth is increasing based on dataset values
+	 * @return whether growth is increasing based on data set values
 	 */
 	public boolean isGrowing() {
 		if (datasets < 2) {
@@ -42,15 +42,43 @@ public class GrowthData {
 		return true;
 	}
 	
+	/**
+	 * Compute differences between data set values and assign to a list [DIFF]
+	 * DIFF.length = datasets.length - 1
+	 * 
+	 * @return list of value differences between data set values
+	 * 	ex. { (li[1] - li[0]), (li[2] - li[1]), ... , (li[li.length) - li[li.length - 1]) }
+	 */
+	public ArrayList<Integer> yearToYearDiff() {
+		ArrayList<Integer> DIFF = new ArrayList<Integer>();
+		if (datasets < 2) return DIFF;
+		
+		int prev = growthValues.get(0);
+		for (int i = 0; i < datasets - 1; ++i) {
+			int curr = growthValues.get(i + 1);
+			DIFF.add(curr - prev);
+			prev = curr;
+		}
+		
+		return DIFF;
+	}
 	
 	public static void main(String args[]) {
 		
-		ArrayList<Integer> list = new ArrayList<>();
-		list.add(1);
-		list.add(4);
-		list.add(2);
+		ArrayList<Integer> li = new ArrayList<Integer>();
+		li.add(157);
+		li.add(432);
+		li.add(2136);
+		li.add(824);
+		li.add(4507);
+			
+		for (Integer i : li) System.out.print(i + " ");
 		
-		for (Integer i : list) System.out.print(i + " ");
+		GrowthData gd = new GrowthData(li);
+		
+		System.out.println("\n" + gd.isGrowing());
+		
+		System.out.println(gd.yearToYearDiff());
 		
 	}
 	
