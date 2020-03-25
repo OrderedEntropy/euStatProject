75b4728accaa71b30556527d828adab8f8612da3
JasonAg <jason.aguilera.s@gmail.com>
2020-03-24 21:01:55 -0400

    Modified to work with Doubles instead of Integers

--- src/main/java/data/GrowthData.java
+++ src/main/java/data/GrowthData.java
@@ -13,10 +13,10 @@ import java.util.ArrayList;
  */
 public class GrowthData {
 	
-	private ArrayList<Integer> growthValues;
+	private ArrayList<Double> growthValues;
 	private int datasets;
 	
-	public GrowthData(ArrayList<Integer> growthValues) {
+	public GrowthData(ArrayList<Double> growthValues) {
 		this.growthValues = growthValues;
 		datasets = growthValues.size();
 	}
@@ -30,9 +30,9 @@ public class GrowthData {
 		if (datasets < 2) {
 			return false;
 		}
-		int prev = growthValues.get(0);
+		double prev = growthValues.get(0);
 		for (int i = 1; i < datasets; ++i) {
-			int curr = growthValues.get(i);
+			double curr = growthValues.get(i);
 			if (prev >= curr) {
 				return false;
 			} else {
@@ -49,13 +49,13 @@ public class GrowthData {
 	 * @return list of value differences between data set values
 	 * 	ex. { (li[1] - li[0]), (li[2] - li[1]), ... , (li[li.length) - li[li.length - 1]) }
 	 */
-	public ArrayList<Integer> yearToYearDiff() {
-		ArrayList<Integer> DIFF = new ArrayList<Integer>();
+	public ArrayList<Double> yearToYearDiff() {
+		ArrayList<Double> DIFF = new ArrayList<Double>();
 		if (datasets < 2) return DIFF;
 		
-		int prev = growthValues.get(0);
+		double prev = growthValues.get(0);
 		for (int i = 0; i < datasets - 1; ++i) {
-			int curr = growthValues.get(i + 1);
+			double curr = growthValues.get(i + 1);
 			DIFF.add(curr - prev);
 			prev = curr;
 		}
@@ -65,14 +65,14 @@ public class GrowthData {
 	
 	public static void main(String args[]) {
 		
-		ArrayList<Integer> li = new ArrayList<Integer>();
-		li.add(157);
-		li.add(432);
-		li.add(2136);
-		li.add(824);
-		li.add(4507);
+		ArrayList<Double> li = new ArrayList<Double>();
+		li.add(157.1);
+		li.add(432.2);
+		li.add(2136.3);
+		li.add(824.4);
+		li.add(4507.5);
 			
-		for (Integer i : li) System.out.print(i + " ");
+		for (Double i : li) System.out.print(i + " ");
 		
 		GrowthData gd = new GrowthData(li);
 		
