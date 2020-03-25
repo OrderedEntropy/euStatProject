15321f27f46e88613c5dd49d3b6d8ed827f513bf
OrderedEntropy <30391434+OrderedEntropy@users.noreply.github.com>
2020-03-24 21:17:12 -0400

    update main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,11 +6,27 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import data.GrowthData;
+
 public class Main {
 	public static void main(String[] args) {
 		try {
 			ArrayList<Double> a = ReadData.readData();
-			System.out.print(a.toString());
+			GrowthData growth = new GrowthData(a);
+			ArrayList<Double> diff = growth.yearToYearDiff();
+			
+			//reporting here
+			
+			int startingYear = 2009;
+			for(int i = 0; i < a.size(); i++) {
+				System.out.printf("%-30.30s %-30.30s %-30.30s  %-30.30s%n", a.get(i), a.get(i), a.get(i), a.get(i));
+//				System.out.print((startingYear + i) + " : " + a.get(i) + "\t");
+//				if(i != 0) {
+//					System.out.print(diff.get(i - 1));
+//				}
+//				System.out.println();
+			}
+			
 		} catch (Exception e) {
 			System.out.print("Couldn't parse data.");
 		}
